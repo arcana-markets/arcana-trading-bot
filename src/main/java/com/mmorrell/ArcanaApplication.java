ab662788d7680f2681f4aaa22dcb103cf0e62236
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-10-07 16:52:38 -0700

    markets.json: Read on startup PoC

--- src/main/java/com/mmorrell/ArcanaApplication.java
+++ src/main/java/com/mmorrell/ArcanaApplication.java
@@ -1,11 +1,24 @@
 package com.mmorrell;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Resources;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
+@Slf4j
 public class ArcanaApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ArcanaApplication.class, args);
+        String markets = Resources.toString(Resources.getResource("static/js/markets.json"), StandardCharsets.UTF_8);
+        List<Map<String, Object>> map = new ObjectMapper().readValue(markets, new TypeReference<>() {});
+        log.info(map.toString());
     }
 }
