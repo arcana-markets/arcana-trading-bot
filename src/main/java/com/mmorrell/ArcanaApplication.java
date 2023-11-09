a74292892f5a90fb00925b015d48d402aa0186e5
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-11-09 09:25:40 -0800

    Add tray icon

--- src/main/java/com/mmorrell/ArcanaApplication.java
+++ src/main/java/com/mmorrell/ArcanaApplication.java
@@ -3,9 +3,12 @@ package com.mmorrell;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
+import com.mmorrell.config.ArcanaTrayIcon;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -15,7 +18,14 @@ import java.util.Map;
 @SpringBootApplication
 @Slf4j
 public class ArcanaApplication {
+
+    public static ConfigurableApplicationContext context;
     public static void main(String[] args) throws IOException {
-        SpringApplication.run(ArcanaApplication.class, args);
+        //SpringApplication.run(ArcanaApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(ArcanaApplication.class);
+        builder.headless(false);
+        context = builder.run(args);
+
+        ArcanaTrayIcon trayIcon = new ArcanaTrayIcon();
     }
 }
---
+++ src/main/java/com/mmorrell/config/ArcanaTrayIcon.java
@@ -0,0 +1,88 @@
+package com.mmorrell.config;
+
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+import com.google.common.io.Resources;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+@Slf4j
+public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware {
+
+    private ApplicationContext context;
+    private static final String IMAGE_PATH = "static/img/brand/man-mage.png";
+    private static final String TOOLTIP = "Running";
+    private PopupMenu popup;
+    final SystemTray tray;
+
+    public ArcanaTrayIcon() {
+        super(createImage(IMAGE_PATH, TOOLTIP), TOOLTIP);
+        popup = new PopupMenu();
+        tray = SystemTray.getSystemTray();
+        try {
+            setup();
+        } catch (AWTException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    @PostConstruct
+    private void setup() throws AWTException {
+        // Create a pop-up menu components
+        MenuItem exitItem = new MenuItem("Exit");
+        popup.add(exitItem);
+        exitItem.addActionListener(e -> {
+            final int exitCode = 0;
+            ExitCodeGenerator exitCodeGenerator = () -> exitCode;
+            tray.remove(ArcanaTrayIcon.this);
+            SpringApplication.exit(context, exitCodeGenerator);
+        });
+        // popup.addSeparator();
+        setPopupMenu(popup);
+        tray.add(this);
+
+
+    }
+
+    protected static Image createImage(String path, String description) {
+
+        URL imageURL = ArcanaTrayIcon.class.getResource(path);
+        byte[] imageData = new byte[0];
+        try {
+            imageData = Resources.toByteArray(Resources.getResource("static/img/brand/man-mage.png"));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        if (imageURL == null) {
+            log.error("Failed Creating Image. Resource not found: " + path);
+            return null;
+        } else {
+            log.info("Loaded image: " + path);
+            return new ImageIcon(imageData, description).getImage();
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+}
