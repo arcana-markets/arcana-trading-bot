b6cc89802d8fae1da8d45dd0431c9450c4dabb11
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-11-09 18:40:25 -0800

    Tray Icon: Fix implementation, add small-mage icon

--- src/main/java/com/mmorrell/config/ArcanaTrayIcon.java
+++ src/main/java/com/mmorrell/config/ArcanaTrayIcon.java
@@ -14,21 +14,26 @@ import java.net.URL;
 import javax.swing.ImageIcon;
 
 import com.google.common.io.Resources;
+import com.mmorrell.ArcanaApplication;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware {
 
     private ApplicationContext context;
-    private static final String IMAGE_PATH = "static/img/brand/man-mage.png";
-    private static final String TOOLTIP = "Running";
+    private static final String IMAGE_PATH = "static/img/brand/small-mage.png";
+    private static final String TOOLTIP = "Arcana: Running";
     private PopupMenu popup;
     final SystemTray tray;
 
@@ -44,6 +49,11 @@ public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware 
         }
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
 
     @PostConstruct
     private void setup() throws AWTException {
@@ -54,7 +64,7 @@ public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware 
             final int exitCode = 0;
             ExitCodeGenerator exitCodeGenerator = () -> exitCode;
             tray.remove(ArcanaTrayIcon.this);
-            SpringApplication.exit(context, exitCodeGenerator);
+            SpringApplication.exit(ArcanaApplication.context, exitCodeGenerator);
         });
         // popup.addSeparator();
         setPopupMenu(popup);
@@ -64,11 +74,9 @@ public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware 
     }
 
     protected static Image createImage(String path, String description) {
-
-        URL imageURL = Resources.getResource("static/img/brand/man-mage.png");
         byte[] imageData = new byte[0];
         try {
-            imageData = Resources.toByteArray(Resources.getResource("static/img/brand/man-mage.png"));
+            imageData = Resources.toByteArray(Resources.getResource(IMAGE_PATH));
             log.info("Loaded image: " + path);
         } catch (IOException e) {
             log.error("Failed creating tray image: " + e.getMessage());
@@ -76,8 +84,4 @@ public class ArcanaTrayIcon extends TrayIcon implements ApplicationContextAware 
         return new ImageIcon(imageData, description).getImage();
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
-    }
 }
